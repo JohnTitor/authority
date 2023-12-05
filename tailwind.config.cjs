c3ba3f999334c2356a9cdc36a80f10c675fc5850
Yuki Okushi <jtitor@2k36.org>
2023-12-05 23:26:04 +0900

    Migrate to Biome

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,12 +9,12 @@ function withOpacity(variableName) {
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     // Remove the following screen breakpoint or add other breakpoints
     // if one breakpoint is not enough for you
     screens: {
-      sm: "640px",
+      sm: '640px',
     },
 
     // Uncomment the following extend
@@ -23,43 +23,43 @@ module.exports = {
     // extend: {
     textColor: {
       skin: {
-        base: withOpacity("--color-text-base"),
-        accent: withOpacity("--color-accent"),
-        inverted: withOpacity("--color-fill"),
+        base: withOpacity('--color-text-base'),
+        accent: withOpacity('--color-accent'),
+        inverted: withOpacity('--color-fill'),
       },
     },
     backgroundColor: {
       skin: {
-        fill: withOpacity("--color-fill"),
-        accent: withOpacity("--color-accent"),
-        inverted: withOpacity("--color-text-base"),
-        card: withOpacity("--color-card"),
-        "card-muted": withOpacity("--color-card-muted"),
+        fill: withOpacity('--color-fill'),
+        accent: withOpacity('--color-accent'),
+        inverted: withOpacity('--color-text-base'),
+        card: withOpacity('--color-card'),
+        'card-muted': withOpacity('--color-card-muted'),
       },
     },
     outlineColor: {
       skin: {
-        fill: withOpacity("--color-accent"),
+        fill: withOpacity('--color-accent'),
       },
     },
     borderColor: {
       skin: {
-        line: withOpacity("--color-border"),
-        fill: withOpacity("--color-text-base"),
-        accent: withOpacity("--color-accent"),
+        line: withOpacity('--color-border'),
+        fill: withOpacity('--color-text-base'),
+        accent: withOpacity('--color-accent'),
       },
     },
     fill: {
       skin: {
-        base: withOpacity("--color-text-base"),
-        accent: withOpacity("--color-accent"),
+        base: withOpacity('--color-text-base'),
+        accent: withOpacity('--color-accent'),
       },
-      transparent: "transparent",
+      transparent: 'transparent',
     },
     fontFamily: {
-      mono: ["IBM Plex Mono", "monospace"],
+      mono: ['IBM Plex Mono', 'monospace'],
     },
     // },
   },
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [require('@tailwindcss/typography')],
 };
