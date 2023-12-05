3d41a943b7f1a05fe387690e590579b1706e82ab
Yuki Okushi <jtitor@2k36.org>
2023-12-05 23:35:23 +0900

    Fix lint

--- src/components/Datetime.tsx
+++ src/components/Datetime.tsx
@@ -16,8 +16,8 @@ export default function Datetime({ datetime, size = 'sm', className }: Props) {
         } inline-block h-6 w-6 fill-skin-base`}
         aria-hidden="true"
       >
-        <path d="M7 11h2v2H7zm0 4h2v2H7zm4-4h2v2h-2zm0 4h2v2h-2zm4-4h2v2h-2zm0 4h2v2h-2z"></path>
-        <path d="M5 22h14c1.103 0 2-.897 2-2V6c0-1.103-.897-2-2-2h-2V2h-2v2H9V2H7v2H5c-1.103 0-2 .897-2 2v14c0 1.103.897 2 2 2zM19 8l.001 12H5V8h14z"></path>
+        <path d="M7 11h2v2H7zm0 4h2v2H7zm4-4h2v2h-2zm0 4h2v2h-2zm4-4h2v2h-2zm0 4h2v2h-2z" />
+        <path d="M5 22h14c1.103 0 2-.897 2-2V6c0-1.103-.897-2-2-2h-2V2h-2v2H9V2H7v2H5c-1.103 0-2 .897-2 2v14c0 1.103.897 2 2 2zM19 8l.001 12H5V8h14z" />
       </svg>
       <span className="sr-only">Posted on:</span>
       <span className={`italic ${size === 'sm' ? 'text-sm' : 'text-base'}`}>
--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -49,7 +49,8 @@ export default function SearchBar({ searchList }: Props) {
     if (searchStr) setInputVal(searchStr);
 
     // put focus cursor at the end of the string
-    setTimeout(function () {
+    setTimeout(() => {
+      // biome-ignore lint: FIXME: inputRef.current can be null
       inputRef.current!.selectionStart = inputRef.current!.selectionEnd =
         searchStr?.length || 0;
     }, 50);
@@ -58,7 +59,7 @@ export default function SearchBar({ searchList }: Props) {
   useEffect(() => {
     // Add search result only if
     // input value is more than one character
-    let inputResult = inputVal.length > 1 ? fuse.search(inputVal) : [];
+    const inputResult = inputVal.length > 1 ? fuse.search(inputVal) : [];
     setSearchResults(inputResult);
 
     // Update search string in URL
@@ -66,19 +67,19 @@ export default function SearchBar({ searchList }: Props) {
       const searchParams = new URLSearchParams(window.location.search);
       searchParams.set('q', inputVal);
       const newRelativePathQuery =
-        window.location.pathname + '?' + searchParams.toString();
+        `${window.location.pathname}?${searchParams.toString()}`;
       history.replaceState(history.state, '', newRelativePathQuery);
     } else {
       history.replaceState(history.state, '', window.location.pathname);
     }
-  }, [inputVal]);
+  }, [inputVal, fuse]);
 
   return (
     <>
       <label className="relative block">
         <span className="absolute inset-y-0 left-0 flex items-center pl-2 opacity-75">
           <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-            <path d="M19.023 16.977a35.13 35.13 0 0 1-1.367-1.384c-.372-.378-.596-.653-.596-.653l-2.8-1.337A6.962 6.962 0 0 0 16 9c0-3.859-3.14-7-7-7S2 5.141 2 9s3.14 7 7 7c1.763 0 3.37-.66 4.603-1.739l1.337 2.8s.275.224.653.596c.387.363.896.854 1.384 1.367l1.358 1.392.604.646 2.121-2.121-.646-.604c-.379-.372-.885-.866-1.391-1.36zM9 14c-2.757 0-5-2.243-5-5s2.243-5 5-5 5 2.243 5 5-2.243 5-5 5z"></path>
+            <path d="M19.023 16.977a35.13 35.13 0 0 1-1.367-1.384c-.372-.378-.596-.653-.596-.653l-2.8-1.337A6.962 6.962 0 0 0 16 9c0-3.859-3.14-7-7-7S2 5.141 2 9s3.14 7 7 7c1.763 0 3.37-.66 4.603-1.739l1.337 2.8s.275.224.653.596c.387.363.896.854 1.384 1.367l1.358 1.392.604.646 2.121-2.121-.646-.604c-.379-.372-.885-.866-1.391-1.36zM9 14c-2.757 0-5-2.243-5-5s2.243-5 5-5 5 2.243 5 5-2.243 5-5 5z" />
           </svg>
         </span>
         <input
@@ -92,7 +93,6 @@ export default function SearchBar({ searchList }: Props) {
           value={inputVal}
           onChange={handleChange}
           autoComplete="off"
-          autoFocus
           ref={inputRef}
         />
       </label>
@@ -108,8 +108,7 @@ export default function SearchBar({ searchList }: Props) {
       )}
 
       <ul>
-        {searchResults &&
-          searchResults.map(({ item, refIndex }) => (
+        {searchResults?.map(({ item, refIndex }) => (
             <Card
               href={`/posts/${slugify(item.data)}`}
               frontmatter={item.data}
