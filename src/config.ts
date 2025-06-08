ceb433961e8979365efbfefff111730a605084ec
Yuki Okushi <huyuumi.dev@gmail.com>
2025-06-08 15:10:40 +0900

    fix: Fix formatting

--- src/config.ts
+++ src/config.ts
@@ -21,11 +21,7 @@ export const siteConfig: SiteConfig = {
 };
 
 export const navBarConfig: NavBarConfig = {
-	links: [
-		LinkPreset.Home,
-		LinkPreset.Archive,
-		LinkPreset.About,
-	],
+	links: [LinkPreset.Home, LinkPreset.Archive, LinkPreset.About],
 };
 
 export const profileConfig: ProfileConfig = {
